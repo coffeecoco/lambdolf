c221bb945a259951b108f7b92471fb7bbd3e759b
Manager <manager@AcidBurn.local>
2017-08-23 12:02:02 -0500

    sorry for pushing to master

--- 2fa-check/lamda/2facheck.py
+++ 2fa-check/lamda/2facheck.py
@@ -1,17 +1,3 @@
 import boto3
 
-session = boto3.Session(
-    profile_name='evolveacademy',
-    region_name='us-west2'
-)
-
-cloudwatch = boto3.client('cloudwatch')
-
-iam = boto3.resource('iam')
-
-
-response = cloudwatch.list_metrics(
-    Namespace='LogMetrics'
-)
-
-print(response)
+# insert code here :)
--- 2fa-check/test/2facheck.py
+++ 2fa-check/test/2facheck.py
@@ -1,17 +1,47 @@
 import boto3
 
+# using local creds for testing
 session = boto3.Session(
     profile_name='evolveacademy',
     region_name='us-west2'
 )
 
-cloudwatch = boto3.client('cloudwatch')
 
-iam = boto3.resource('iam')
+iam = boto3.client('iam')
 
 
-response = cloudwatch.list_metrics(
-    Namespace='LogMetrics'
+# TODO Get IAM user and create a virtual device for them
+
+response = iam.create_virtual_mfa_device(
+    Path='/',
+    VirtualMFADeviceName='jholcomb@evolvesecurity.ioMFADevice'
 )
 
+
+# TODO Parse out key params so we use in able
+serial_number = response['VirtualMFADevice']['SerialNumber']
+
+# For Debug
 print(response)
+print("\n\n")
+
+
+# TODO Add Enabled MFA boto call
+response = client.enable_mfa_device(
+    UserName = 'string', # fix
+    SerialNumber = serial_number,
+    AuthenticationCode1 = 'string', # fix
+    AuthenticationCode2 = 'string' # fix
+)
+
+
+
+# Delete IAM so we can keep running the script
+iam.delete_virtual_mfa_device(
+    SerialNumber = serial_number
+)
+
+# print responses to console
+
+print(response)
+print("\n\n")
